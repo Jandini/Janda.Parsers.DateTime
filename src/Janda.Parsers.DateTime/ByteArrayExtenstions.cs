e52a6811bdd68af2670aaec24c8486fb470718d5
Matt <matt.janda.kingston@gmail.com>
2020-05-18 22:50:56 +0100

    feat: Add cdate time and hfs file time parsers

---
+++ src/Janda.Parsers.DateTime/ByteArrayExtenstions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Janda.Parsers
+{
+    public static class ByteArrayExtenstions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static DateTime AsHFSFileTime(this byte[] buffer, int index)
+        {            
+            return DateTimeParser.HFSDateTimeEpoch.AddSeconds((uint)((buffer[index++] << 24) | (buffer[index++] << 16) | (buffer[index++] << 8) | buffer[index++]));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static DateTime AsCDateTime(this byte[] buffer, int index)
+        {
+            return DateTimeParser.CDateTimeEpoch.AddSeconds(BitConverter.ToUInt32(buffer, index));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static DateTime AsCDateTimeBE(this byte[] buffer, int index)
+        {
+            return DateTimeParser.CDateTimeEpoch.AddSeconds((uint)((buffer[index++] << 24) | (buffer[index++] << 16) | (buffer[index++] << 8) | buffer[index++]));
+        }
+    }      
+}
+ 
---
+++ src/Janda.Parsers.DateTime/DateTimeParserExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Janda.Parsers
+{
+    public static class ByteArrayParserExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static DateTimeParser ParseAt(this byte[] buffer, int index)
+        {
+            return new DateTimeParser(buffer, index);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static DateTimeParser Parse(this byte[] buffer)
+        {
+            return new DateTimeParser(buffer);
+        }
+    }
+}
+ 
